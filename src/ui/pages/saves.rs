4039e98431e1092571d03247f89e0a4ed5eb7c2e
lwb <lwb-2021@qq.com>
2026-02-15 21:25:55 +0800

    fix(ui): avoid ui from stucking when choosing snapshot to recover

--- src/ui/pages/saves.rs
+++ src/ui/pages/saves.rs
@@ -1,8 +1,12 @@
-use std::{cell::RefCell, sync::Arc};
+use std::{
+    cell::RefCell,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 use gtk4::{
-    Box, Button, Image, Label, Window,
-    glib::object::Cast,
+    Box, Button, Image, Label, Spinner, Window,
+    glib::{MainContext, object::Cast},
     prelude::{BoxExt, ButtonExt, EditableExt, GtkWindowExt, WidgetExt},
 };
 use rustic_core::{SnapshotOptions, repofile::SnapshotFile};
@@ -96,52 +100,72 @@ pub fn saves() -> Box {
         let id = id0.clone();
 
         recover_button.connect_clicked(move |_| {
-            let inner = dialog_wrapper();
-            inner.set_valign(gtk4::Align::Fill);
-
+            let save = &AppState::instance().saves[&id];
             let cancel_btn = Button::with_label(&t!("messages.cancel").to_string());
-
             cancel_btn.connect_clicked(|btn| {
                 let window: Window = btn.root().unwrap().dynamic_cast().unwrap();
                 window.close();
             });
-
-            let save = &AppState::instance().saves[&id];
-
-            for snapshot in save.list_backups().unwrap_or_default() {
-                let snapshot_card = Box::builder().build();
-                snapshot_card.append(&title(snapshot.label.clone()));
-                let btn = Button::builder().child(&snapshot_card).build();
-
-                let id = id.clone();
-
-                let save_name = save.name.clone();
-
-                btn.connect_clicked(move |_| {
-                    tasks::spawn(
-                        format!(
-                            "{}: {}/{}",
-                            t!("pages.saves.recover"),
-                            save_name,
-                            snapshot.label.clone()
-                        ),
-                        TaskInfo::Recover {
-                            id: id.clone(),
-                            snapshot: snapshot.clone(),
-                        },
-                    );
-                });
-                inner.append(&btn);
-            }
-
-            inner.append(&cancel_btn);
-
+            let inner = dialog_wrapper();
+            inner.set_valign(gtk4::Align::Fill);
+            let spinner = Spinner::new();
+            inner.append(&spinner);
             let dialog = Window::builder()
                 .title(t!("pages.saves.backup"))
                 .child(&inner)
                 .modal(true)
                 .build();
             dialog.present();
+            spinner.start();
+
+            let data = Arc::new(Mutex::new(None));
+            let data_ref = data.clone();
+            let id = id.clone();
+            let save = save.clone();
+            let save_name0 = save.name.clone();
+
+            thread::spawn(move || {
+                let mut data = data_ref.lock().unwrap();
+                *data = Some(save.list_backups().unwrap_or_default());
+                drop(data);
+            });
+
+            gtk4::glib::source::idle_add_local(move || {
+                while let Err(_) = data.try_lock() {
+                    return gtk4::glib::ControlFlow::Continue;
+                }
+                spinner.stop();
+                if let Some(data) = data.lock().unwrap().as_ref() {
+                    for snapshot in data {
+                        let snapshot_card = Box::builder().build();
+                        snapshot_card.append(&title(snapshot.label.clone()));
+                        let btn = Button::builder().child(&snapshot_card).build();
+
+                        let id = id.clone();
+                        let save_name = save_name0.clone();
+                        let snapshot = snapshot.clone();
+                        btn.connect_clicked(move |_| {
+                            tasks::spawn(
+                                format!(
+                                    "{}: {}/{}",
+                                    t!("pages.saves.recover"),
+                                    save_name,
+                                    snapshot.label
+                                ),
+                                TaskInfo::Recover {
+                                    id: id.clone(),
+                                    snapshot: snapshot.clone(),
+                                },
+                            );
+                        });
+                        inner.append(&btn);
+                    }
+
+                    inner.remove(&spinner);
+                    inner.append(&cancel_btn);
+                }
+                gtk4::glib::ControlFlow::Break
+            });
         });
 
         button_box.append(&Label::builder().hexpand(true).build());
